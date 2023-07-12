026a076a444e26c1d3de633b439288156d76974f
vmk0316 <101328896+vmk0316@users.noreply.github.com>
2023-07-12 16:08:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,4 +15,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+subscription_id ="277e7d4a-a5db-4400-a048-dd676727f1e0"
+client_id       = "62b184eb-be26-4ca8-b7cb-1dfff77802ab"
+client_secret   = "_Pp8Q~Aez4uck_GdFfDriNw80_j5rrapKbLZYbmd"
+tenant_id       = "6dc82359-2687-4af5-a320-bb547ec40204"
 }
